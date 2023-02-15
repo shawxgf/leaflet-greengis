837ad2da1da30cd7f7d0d8da11f3dd6b59da3bfa
shawxgf <xgf05191995@gmail.com>
2023-02-15 21:53:02 +0800

    无法显示地图，暂不知道原因，烦啊

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Import stylesheets
-import './style.css';
-import { Map, TileLayer, LayerGroup} from 'leaflet';
+// import './style.css';
+// import { Map, TileLayer, LayerGroup} from 'leaflet';
 
 // Write Javascript code!
 const map = new Map('map');
 
 const amapLayer = new TileLayer(
-  // 'http://wprd0{s}.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zh_cn&size=1&scl=1&style=7',
-  'http://wprd04.is.autonavi.com/appmaptile?lang=zh_cn&size=1&style=7&x={x}&y={y}&z={z}',
+  'http://wprd0{s}.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zh_cn&size=1&scl=1&style=7',
+  // 'http://wprd04.is.autonavi.com/appmaptile?lang=zh_cn&size=1&style=7&x={x}&y={y}&z={z}',
   {
     subdomains: '1234'
   }
@@ -25,23 +25,33 @@ const tdtLabelLayer = new TileLayer(
 
 const tdtLayer = new LayerGroup([tdtVectorLayer, tdtLabelLayer])
 
-tdtLayer.addTo(map);
+amapLayer.addTo(map);
 
 map.setView([39.909186, 116.397411], 10);
 
-const items = document.getElementsByName('base')
-items.forEach(item => {
-  item.onclick = (evt) => {
-    switch (evt.target.value)
-    {
-      case 'amap':
-        tdtLayer.removeFrom(map);
-        amapLayer.addTo(map);
-        break;
-      case 'tdt':
-        amapLayer.removeFrom(map);
-        tdtLayer.removeFrom(map);
-        break;
-    }
-  }
-});
+// const items = document.getElementsByName('base')
+// items.forEach(item => {
+//   item.onclick = (evt) => {
+//     switch (evt.target.value)
+//     {
+//       case 'amap':
+//         tdtLayer.removeFrom(map);
+//         amapLayer.addTo(map);
+//         break;
+//       case 'tdt':
+//         amapLayer.removeFrom(map);
+//         tdtLayer.removeFrom(map);
+//         break;
+//     }
+//   }
+// });
+
+const layerControl = new Control.Layers(
+  {
+    高德: amapLayer,
+    天地图: tdtLayer
+  },
+  {},
+  { collapsed: false }
+);
+layerControl.addTo(map);

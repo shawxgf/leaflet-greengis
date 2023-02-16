58230495bb89d512e8b5816ec6001e3f25136302
shawxgf <42833495+shawxgf@users.noreply.github.com>
2023-02-16 14:14:38 +0800

    3 files modified

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 // Import stylesheets
 import './style.css';
-import { Map, TileLayer, LayerGroup, Control} from 'leaflet';
+import { Map, TileLayer, LayerGroup, Control } from 'leaflet';
+
 
 // Write Javascript code!
 const map = new Map('map');
 
 const amapLayer = new TileLayer(
-  // 'http://wprd0{s}.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zh_cn&size=1&scl=1&style=7',
-  'http://wprd04.is.autonavi.com/appmaptile?lang=zh_cn&size=1&style=7&x={x}&y={y}&z={z}',
+  'http://wprd0{s}.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zh_cn&size=1&scl=1&style=7',
+  // 'http://wprd04.is.autonavi.com/appmaptile?lang=zh_cn&size=1&style=7&x={x}&y={y}&z={z}',
   {
     subdomains: '1234'
   }
